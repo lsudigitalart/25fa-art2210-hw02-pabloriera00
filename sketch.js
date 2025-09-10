c61835c900646c47d359ca66b479b93c25ff114d
pabloriera00 <priera1@lsu.edu>
2025-09-10 12:37:25 -0500

    commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,139 @@
 function setup() {
   createCanvas(400, 269);
+  background(125)
 }
 
 function draw() {
+  fill(120, 20,20 );
+  rect(0,120, 500, 180 );
+ 
+  fill(205, 173, 0 );
+  rect(0,-15, 500, 170 );
+
+  fill(0);
+  rect(0,150, 500, 8 );
+
+  fill(0);
+  rect(0,190, 500, 8 );
+
+  fill(0);
+  rect(0,220, 500, 8 );
+
+  fill(0);
+  ellipse(100, 147,49, 6);
+
+  rotate(PI/70);
+  fill(0);
+  ellipse(107, 138,50, 6);
+
+  fill(0);
+  ellipse(108, 133,50, 12);
+  fill(0);
+  ellipse(87, 133,20, 4);
+
+  fill(0);
+  ellipse(108, 128,49, 7);
+
+  fill(0);
+  ellipse(114, 125,49, 6);
+
+  fill(0);
+  ellipse(212, 137,52, 7);
+
+  fill(0); 
+  triangle(width - 100,40, width - 50, 10, width - 50, 70);
+
+  rotate (PI/60)
+  fill(0);
+  ellipse(345, 40,30, 4);
+
+  fill(0);
+  ellipse(220,35,80, 80);
+
+  fill(0);
+  ellipse(250, 80,40, 40);
+
+  rotate(PI/45);
+  fill(0);
+  ellipse(75,70, 80,60);
+
+  rotate(PI/45);
+  fill(205, 173, 0 );
+  ellipse(80,65, 45,60);
+
+  rotate(PI/-70);
+  fill(0);
+  ellipse(210, 141,52, 7);
+
+  rotate(PI/50)
+  fill(0);
+  ellipse(225, 123,60, 3);
+
+  fill(0);
+  ellipse(8, 5,49, 7);
+
+  rotate(PI/50);
+  fill(0);
+  ellipse(3, 10,49, 7);
+
+ rotate(PI/65);
+  fill(0);
+  ellipse(-3, 15,49, 7);
+
+  fill(0);
+  ellipse(3, 3,49, 20);
+
+  rotate(PI/50);
+  fill(0);
+  ellipse(15, 1,49, 4);
+
+  rotate(PI/65);
+  fill(0);
+  ellipse(-8, 20,49, 7);
+
+  fill(120, 20,20 );
+  ellipse(240,70,60,20);
+  noStroke();
+
+  rotate(PI/-100);
+  fill(0);
+  ellipse(250,-35,49,7);
+  rotate(PI/-100);
+  fill(0);
+  ellipse(250,-35,49,10);
+  rotate(PI/-100);
+  fill(0);
+  ellipse(250,-35,49,12);
+   rotate(PI/-100);
+  fill(0);
+  ellipse(250,-38,49,14);
+
+   fill(0);
+  ellipse(190,-38,49,14);
+  fill(0);
+  ellipse(190,-28,49,7);
+  fill(0);
+  ellipse(190,-20,49,10);  
+  fill(0);
+  ellipse(190,-14,49,10); 
+  fill(0);
+  ellipse(190,-10,49,5); 
+  
+  fill(0);
+  ellipse(390, -30, 25, 25);
+
+  fill(0); 
+  triangle(width - 70,-110, width - 90, -80, width - 15, -75);
+
+fill(0);
+ellipse(110,-20,70,60);
+
+fill(0);
+ellipse(350,15,40,40);
+
+fill(0);
+ellipse(310,1,45,45);
+
+
 
 }
